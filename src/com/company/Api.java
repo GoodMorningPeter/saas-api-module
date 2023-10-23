faff230122c833b513ae54b539a8b795c93bc487
GoodMorningPeter <shuxuejianmo202202@163.com>
2023-10-23 23:24:26 +0800

    update

---
+++ src/com/company/Api.java
@@ -0,0 +1,12 @@
+package com.company;
+
+public class Api {
+    private String serviceCode;
+    private String appKey;
+    private String requestTime;
+    private String sign;
+    private String data;
+
+    // getters and setters
+    // ...
+}
---
+++ src/com/company/ApiResponse.java
@@ -0,0 +1,11 @@
+package com.company;
+
+public class ApiResponse {
+    private String executeResult;
+    private String errorCode;
+    private String errorMessage;
+    private String data;
+
+    // getters and setters
+    // ...
+}
