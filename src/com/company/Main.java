aa48dffa5b87ff045eecc299c6a6d132e525ad8c
GoodMorningPeter <shuxuejianmo202202@163.com>
2023-10-31 17:37:44 +0800

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -13,20 +13,7 @@ public class Main {
 
 
     public static void main(String[] args) throws IOException, InterruptedException {
-	// write your code here
-//        ApiManager apiManager = new MemoryApiManager();
-//        ApiErrorHandler errorHandler = new ConsoleApiErrorHandler();
-//        ApiInvoker apiInvoker = new HttpClientApiInvoker(apiManager, errorHandler);
-//
-//        Api weatherApi = new Api();
-//        weatherApi.setServiceCode("weather");
-//        weatherApi.setApiUrl("http://t.weather.itboy.net/api/weather/city/101250601");
-//        apiManager.registApi(weatherApi);
-//        ApiRequest apiRequest = new ApiRequest();
-//        apiRequest.setServiceCode("weather");
-//        ApiResponse apiResponse = apiInvoker.invokeApi(apiRequest);
-//        errorHandler.logApiCall(apiRequest, apiResponse);
-
+        
         ApiManager apiManager = new MemoryApiManager();
 
         // Register WeatherApi
